2787b047923c76a49d7a6265c37b89cd58fb0723
Amine Dassouki <amine.dassouki@gmail.com>
2017-09-05 20:48:36 +0000

    Styling Campgrounds

--- v1/app.js
+++ v1/app.js
@@ -8,7 +8,14 @@ app.set("view engine", "ejs");
 var campgrounds = [
     {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},    
     {name: "Bowser Point", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"},    
-    {name: "Yeti's Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"}    
+    {name: "Yeti's Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
+    {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},    
+    {name: "Bowser Point", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"},    
+    {name: "Yeti's Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
+    {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},    
+    {name: "Bowser Point", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"},    
+    {name: "Yeti's Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"}
+    
 ];
 
 app.get("/", function(req, res){
