dc8585d1f45b2e30db8b85fd7ada642533ae1aa1
Amine Dassouki <amine.dassouki@gmail.com>
2017-09-07 19:48:51 +0000

    added mongoose

--- v2/app.js
+++ v2/app.js
@@ -1,29 +1,49 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose")
 
+mongoose.connect("mongodb://localhost/yelp_camp")
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},    
-    {name: "Bowser Point", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"},    
-    {name: "Yeti's Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-    {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},    
-    {name: "Bowser Point", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"},    
-    {name: "Yeti's Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-    {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},    
-    {name: "Bowser Point", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"},    
-    {name: "Yeti's Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"}
-    
-];
+// Schema setup
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Bowser Point", 
+//         image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"
+        
+//     }, 
+//     function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("Newly created campground: ");
+//             console.log(campground);
+//         }
+//     });
+
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res){
@@ -31,9 +51,14 @@ app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    // redirect back to camgrounds page
-    res.redirect("/campgrounds");
+    // Create new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(res,res){
